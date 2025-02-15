1b6c2085daae5fbcfaf5f3d2201216c5af2430dc
Edy Kend <paleleh@gmail.com>
2025-02-15 11:32:58 +0700

    Update install-odoo17.sh

--- install-odoo17.sh
+++ install-odoo17.sh
@@ -28,7 +28,6 @@ OE_VERSION="17.0"
 # Set this to True if you want to install the Odoo enterprise version!
 IS_ENTERPRISE="False"
 # Installs postgreSQL V14 instead of defaults (e.g V12 for Ubuntu 20/22) - this improves performance
-INSTALL_POSTGRESQL="False"
 INSTALL_POSTGRESQL_FOURTEEN="True"
 # Set this to True if you want to install Nginx!
 INSTALL_NGINX="False"
@@ -51,6 +50,10 @@ ADMIN_EMAIL="odoo@example.com"
 ## in order to have correct version of wkhtmltopdf installed, for a danger note refer to
 ## https://github.com/odoo/odoo/wiki/Wkhtmltopdf ):
 ## https://www.odoo.com/documentation/16.0/administration/install.html
+
+INSTALL_POSTGRESQL="False"
+INSTALL_PYTHON_LIB="False"
+INSTALL_ODOO_requirement="True:
 
 # Check if the operating system is Ubuntu 22.04
 if [[ $(lsb_release -r -s) == "22.04" ]]; then
@@ -79,34 +82,38 @@ sudo apt-get install libpq-dev
 # Install PostgreSQL Server
 #--------------------------------------------------
 echo -e "\n---- Install PostgreSQL Server ----"
-if [ $INSTALL_POSTGRESQL_FOURTEEN = "True" ]; then
-    echo -e "\n---- Installing postgreSQL V14 due to the user it's choise ----"
-    sudo curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
-    sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-    sudo apt-get update
-    sudo apt-get install postgresql-16
-else
-    echo -e "\n---- Installing the default postgreSQL version based on Linux version ----"
-    sudo apt-get install postgresql postgresql-server-dev-all -y
+if [ $INSTALL_POSTGRESQL = "True" ]; then
+    if [ $INSTALL_POSTGRESQL_FOURTEEN = "True" ]; then
+        echo -e "\n---- Installing postgreSQL V14 due to the user it's choise ----"
+        sudo curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+        sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+        sudo apt-get update
+        sudo apt-get install postgresql-16
+    else
+        echo -e "\n---- Installing the default postgreSQL version based on Linux version ----"
+        sudo apt-get install postgresql postgresql-server-dev-all -y
+    fi
+    echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
+    sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 fi
-
-
-echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
-sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 
 #--------------------------------------------------
 # Install Dependencies
 #--------------------------------------------------
-echo -e "\n--- Installing Python 3 + pip3 --"
-sudo apt-get install python3 python3-pip
-sudo apt-get install git python3-cffi build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng-dev libjpeg-dev gdebi -y
+if [ $INSTALL_PYTHON_LIB = "True" ]; then
+    echo -e "\n--- Installing Python 3 + pip3 --"
+    sudo apt-get install python3 python3-pip
+    sudo apt-get install git python3-cffi build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng-dev libjpeg-dev gdebi -y
+if
 
-echo -e "\n---- Install python packages/requirements ----"
-sudo -H pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
-
-echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
-sudo apt-get install nodejs npm -y
-sudo npm install -g rtlcss
+if [ $INSTALL_ODOO_requirement = "True" ]; then
+    echo -e "\n---- Install python packages/requirements ----"
+    sudo -H pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
+    
+    echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
+    sudo apt-get install nodejs npm -y
+    sudo npm install -g rtlcss
+if
 
 #--------------------------------------------------
 # Install Wkhtmltopdf if needed
